85a3585b898abc2c1bf045f8a5c2261b8cdd7c1b
gearprn <gearprn@gmail.com>
2021-04-24 01:03:00 +0700

    :wrench: Add register resolver
    - update login error message
    - rename return payload

--- backend/src/graphql/mutations/auth.js
+++ backend/src/graphql/mutations/auth.js
@@ -4,11 +4,47 @@ import jsonwebtoken from 'jsonwebtoken';
 
 import { UserModel, UserTC } from '../../models';
 
-const LoginPayload = schemaComposer.createObjectTC({
-  name: 'LoginPayload',
+const AuthPayload = schemaComposer.createObjectTC({
+  name: 'AuthPayload',
   fields: {
     token: 'String',
     user: UserTC.getType(),
+  },
+});
+
+export const register = schemaComposer.createResolver({
+  name: 'register',
+  args: {
+    displayName: 'String!',
+    username: 'String!',
+    password: 'String!',
+  },
+  type: AuthPayload,
+  resolve: async ({ args }) => {
+    const { displayName, username, password } = args;
+    const isUsernameAlreadyUse = await UserModel.findOne({ username });
+    if (isUsernameAlreadyUse) {
+      throw new UserInputError(`Username ${username} already used.`);
+    }
+
+    if (password.length <= 7) {
+      throw new UserInputError('Password is too weak.');
+    }
+
+    const user = await UserModel.create({
+      displayName,
+      role: 'customer',
+      username,
+      password,
+    });
+
+    return {
+      token: jsonwebtoken.sign({ _id: user._id }, process.env.SECRET ?? 'default-secret', {
+        expiresIn: '1d',
+        algorithm: 'HS256',
+      }),
+      user,
+    };
   },
 });
 
@@ -18,12 +54,12 @@ export const login = schemaComposer.createResolver({
     username: 'String!',
     password: 'String!',
   },
-  type: LoginPayload,
+  type: AuthPayload,
   resolve: async ({ args }) => {
     const { username, password } = args;
     const user = await UserModel.findOne({ username });
     if (!user) {
-      throw new UserInputError(`Username ${username} not found`);
+      throw new UserInputError('Username not found');
     }
     const valid = await user.verifyPassword(password);
     if (!valid) {
