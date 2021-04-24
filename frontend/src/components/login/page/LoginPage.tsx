e1e5bab13c5f8ad3c7b0aca11e5cabcd2072461a
gearprn <gearprn@gmail.com>
2021-04-25 02:25:02 +0700

    :wrench: Update login page
    - now have a error message display on a modal when fail  to login

--- frontend/src/components/login/page/LoginPage.tsx
+++ frontend/src/components/login/page/LoginPage.tsx
@@ -1,67 +1,112 @@
 import React, { FC, useCallback, useState } from "react";
 import { NavLink } from "react-router-dom";
 
+import Modal from "../../commons/Modal";
 import "./LoginPageStyle.css";
 import { useSession } from "../../../context/SessionContext";
+import useModal from "../../../hooks/useModal";
 
 const Login: FC = () => {
   const { login }: any = useSession();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [title, setTitle] = useState("");
+  const [bodyMessage, setBodyMessage] = useState("");
+  const { isShowing, toggle } = useModal(false);
+
+  const handleErrorMessage = useCallback(
+    (title: string, bodyMessage: string) => {
+      setTitle(title);
+      setBodyMessage(bodyMessage);
+      toggle();
+    },
+    [toggle]
+  );
 
   const handleSubmit = useCallback(
     async (event) => {
       event.preventDefault();
+
+      if (username.length <= 5)
+        return handleErrorMessage(
+          "กรุณาใส่ชื่อผู้ใช้งาน",
+          "ชื่อผู้ใช้งานต้องมีอย่างน้อย 6 ตัวอักษร"
+        );
+
+      // if (password.length <= 7)
+      //   return handleErrorMessage("กรุณาใส่รหัสผ่าน", "รหัสผ่านต้องมีอย่างน้อย 8 ตัวอักษร");
+
       try {
-        await login(username, password);
-      } catch (err) {}
+        const result = await login(username, password);
+        // console.log(result);
+      } catch ({ message }) {
+        console.log(message);
+        if (message === "Username not found")
+          return handleErrorMessage("เข้าสู่ระบบไม่สำเร็จ", "ไม่พบบัญชีในระบบ");
+
+        if (message === "Incorrect password")
+          return handleErrorMessage("เข้าสู่ระบบไม่สำเร็จ", "ชื่อผู้ใช้งานหรือรหัสผ่านไม่ถูกต้อง");
+      }
     },
-    [login, password, username]
+    [handleErrorMessage, login, password, username]
   );
 
-  return (
-    <div className="login-page flex justify-center items-center">
-      <div className="login-box relative p-10">
-        <form onSubmit={handleSubmit}>
-          <p className="text-2xl">เข้าสู่ระบบ</p>
-          <p className="text-xs mt-1">กรอกชื่อผู้ใช้งานและรหัสผ่านเพื่อเข้าใช้งาน</p>
+  const handleCallBack = (stats: boolean) => {
+    if (!stats) toggle();
+  };
 
-          <div className="mt-16">
-            <p className="text-md">ชื่อผู้ใช้งาน</p>
-            <input
-              onChange={(event) => setUsername(event.target.value)}
-              className="form-input w-full bg-white-100 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-dark-200"
-              type="text"
-              placeholder="ชื่อผู้ใช้งาน"
-            />
-            <p className="mt-5 text-md">รหัสผ่าน</p>
-            <input
-              onChange={(event) => setPassword(event.target.value)}
-              className="form-input w-full bg-white-100 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-dark-200"
-              type="password"
-              placeholder="รหัสผ่าน"
-            />
-            <p className="text-right mt-5 text-sm">
-              ยังไม่มีบัญชีหรอ ?{" "}
-              <NavLink to="/register" className="text-blue-link">
-                สมัครสมาชิก
-              </NavLink>
-            </p>
-          </div>
-          <div className="mt-16">
-            <div className="flex justify-center items-center">
-              <div>
-                <button className="bg-transparent hover:border-dark-200 text-blue-700 font-semibold hover:text-white py-2 px-4 border-2  border-gold-500 hover:border-transparent rounded">
-                  เข้าสู่ระบบ
-                </button>
-                <NavLink to="/" className="text-center text-sm mt-5 block text-blue-link">
-                  กลับไปหน้าหลัก
+  return (
+    <div>
+      <Modal
+        isOpen={isShowing}
+        isHasAccept={false}
+        isHasDecline={false}
+        title={title}
+        bodyMessage={bodyMessage}
+        callBackFunction={handleCallBack}
+      />
+      <div className="login-page flex justify-center items-center">
+        <div className="login-box relative p-10">
+          <form onSubmit={handleSubmit}>
+            <p className="text-2xl">เข้าสู่ระบบ</p>
+            <p className="text-xs mt-1">กรอกชื่อผู้ใช้งานและรหัสผ่านเพื่อเข้าใช้งาน</p>
+
+            <div className="mt-16">
+              <p className="text-md">ชื่อผู้ใช้งาน</p>
+              <input
+                onChange={(event) => setUsername(event.target.value)}
+                className="form-input w-full bg-white-100 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-dark-200"
+                type="text"
+                placeholder="ชื่อผู้ใช้งาน"
+              />
+              <p className="mt-5 text-md">รหัสผ่าน</p>
+              <input
+                onChange={(event) => setPassword(event.target.value)}
+                className="form-input w-full bg-white-100 px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-dark-200"
+                type="password"
+                placeholder="รหัสผ่าน"
+              />
+              <p className="text-right mt-5 text-sm">
+                ยังไม่มีบัญชีหรอ ?{" "}
+                <NavLink to="/register" className="text-blue-link">
+                  สมัครสมาชิก
                 </NavLink>
+              </p>
+            </div>
+            <div className="mt-16">
+              <div className="flex justify-center items-center">
+                <div>
+                  <button className="bg-transparent hover:border-dark-200 text-blue-700 font-semibold hover:text-white py-2 px-4 border-2  border-gold-500 hover:border-transparent rounded">
+                    เข้าสู่ระบบ
+                  </button>
+                  <NavLink to="/" className="text-center text-sm mt-5 block text-blue-link">
+                    กลับไปหน้าหลัก
+                  </NavLink>
+                </div>
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
