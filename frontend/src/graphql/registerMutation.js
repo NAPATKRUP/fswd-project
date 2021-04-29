e5e88d182d53df20fd1753e6417da14934c6a9e1
gearprn <gearprn@gmail.com>
2021-04-30 01:24:59 +0700

    :wrench: Update return type to match backend of register mutation

--- frontend/src/graphql/registerMutation.js
+++ frontend/src/graphql/registerMutation.js
@@ -3,12 +3,7 @@ import { gql } from '@apollo/client';
 export const REGISTER_MUTATION = gql`
   mutation($username: String!, $password: String!, $displayName: String!) {
     register(username: $username, password: $password, displayName: $displayName) {
-      token
-      user {
-        _id
-        role
-        displayName
-      }
+      status
     }
   }
 `;
