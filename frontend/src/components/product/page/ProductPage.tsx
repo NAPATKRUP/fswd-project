02b69e128d9cdeb7ef0b7875dc55ca332dbcf4e8
vivi00008 <70990464+vivi00008@users.noreply.github.com>
2021-05-02 01:50:39 +0700

    update ProductPage
    edit logic for search in FilterProductBar

--- frontend/src/components/product/page/ProductPage.tsx
+++ frontend/src/components/product/page/ProductPage.tsx
@@ -1,6 +1,7 @@
-import { FC, lazy, useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { FC, lazy, useState, useCallback, useEffect } from 'react';
+import { useLazyQuery, useQuery } from '@apollo/client';
 import { FILTER_PRODUCT_QUERY } from '../../../graphql/filterProductQuery';
+import ReactPagination from '../components/ReactPagination';
 
 const ContentWithSidebarLayout = lazy(
   () => import('../../commons/layouts/ContentWithSidebarLayout')
@@ -10,44 +11,132 @@ const Navigator = lazy(() => import('../../commons/Navigator'));
 const FilterProductBar = lazy(() => import('../components/FilterProductBar'));
 const ProductWrapper = lazy(() => import('../components/ProductWrapper'));
 
-const ProductPage: FC = () => {
-  const [searchType, setSearchType] = useState<string>('PRICE_ASC');
-  const [name, setName] = useState<string>('');
-  const [minPrice, setMinPrice] = useState<number>(0);
-  const [maxPrice, setMaxPrice] = useState<number>(100000);
+const ProductPage: any = () => {
+  const [searchType, setSearchType] = useState<any>(undefined);
+  const [nameInput, setNameInput] = useState<any>(undefined);
+  const [minPrice, setMinPrice] = useState<any>(undefined);
+  const [maxPrice, setMaxPrice] = useState<any>(undefined);
 
-  const { loading, error, data } = useQuery(FILTER_PRODUCT_QUERY, {
+  const [allProduct, setAllProduct] = useState<any>([]);
+  const [showProduct, setShowProduct] = useState<any>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productPerPage = 1;
+
+  const [queryProduct, { loading, error, data }] = useLazyQuery<any>(FILTER_PRODUCT_QUERY, {
     variables: {
-      typeFilter: searchType,
-      name: name,
-      minPrice: minPrice,
-      maxPrice: maxPrice,
+      typeFilter: 'PRICE_ASC',
+      minPrice: 1,
+      maxPrice: 100000,
+      name: '',
     },
   });
 
-  if (loading) {
+  // inDebounce function
+  // Set inDebounce for use in queryProduct
+  let inDebounce: any;
+  // Set time out for make delay between queryProduct
+  const setTimeOutQuery = (func: any, delay: any) => {
+    inDebounce = setTimeout(func, delay);
+  };
+  // Clear all time out
+  const clearTimeOutQuery = () => {
+    clearTimeout(inDebounce);
+  };
+
+  // Update filter when have event with search
+  useEffect(() => {
+    if (!nameInput && !maxPrice && !minPrice && !searchType) {
+      setSearchType('PRICE_ASC');
+      queryProduct();
+    } else {
+      clearTimeOutQuery();
+      setTimeOutQuery(
+        queryProduct({
+          variables: {
+            typeFilter: searchType,
+            name: nameInput,
+            minPrice: minPrice,
+            maxPrice: maxPrice,
+          },
+        }),
+        300
+      );
+    }
+    setCurrentPage(1);
+  }, [searchType, nameInput, minPrice, maxPrice]);
+
+  // Update all product when data change by new query
+  useEffect(() => {
+    if (data?.filterProduct) {
+      setAllProduct(data?.filterProduct);
+    }
+  }, [data]);
+
+  // Update show product in each page when change page or new query product
+  useEffect(() => {
+    const indexOfLastProduct = currentPage * productPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productPerPage;
+    const currentProduct = allProduct.slice(indexOfFirstProduct, indexOfLastProduct);
+    setShowProduct(currentProduct);
+  }, [currentPage, allProduct]);
+
+  const handleSearchType = useCallback((e) => {
+    setSearchType(e.target.value);
+  }, []);
+
+  const handleName = useCallback((e) => {
+    setNameInput(e.target.value);
+  }, []);
+
+  const handleMaxPrice = useCallback((e) => {
+    setMaxPrice(parseInt(e.target.value));
+  }, []);
+
+  const handleMinPrice = useCallback((e) => {
+    setMinPrice(parseInt(e.target.value));
+  }, []);
+
+  const paginate = useCallback((pageNumber: any) => setCurrentPage(pageNumber), []);
+
+  if (
+    loading &&
+    nameInput === undefined &&
+    minPrice === undefined &&
+    maxPrice === undefined &&
+    searchType === undefined
+  ) {
     return <Loading />;
   }
-  if (error) {
-    console.log(error);
-  }
-  //const { filterProduct } = data;
 
-  const handleCallBack = (searchType: string, name: string, minPrice: number, maxPrice: number) => {
-    setSearchType(searchType);
-    setName(name);
-    setMinPrice(minPrice);
-    setMaxPrice(maxPrice);
-  };
+  if (error) {
+    return alert(error);
+  }
 
   return (
     <ContentWithSidebarLayout>
-      <Navigator listOfNode={['หน้าหลัก', '>>', 'สินค้า']} />
-      <div className="flex flex-col items-center lg:px-20 md:px-10 py-10">
-        <FilterProductBar callBackFunction={handleCallBack} />
-        <div className="w-full border-b-4 border-gold-200 rounded-full my-8"></div>
-        <ProductWrapper product={data?.filterProduct} />
-        <div className="w-full border-b-4 border-gold-200 rounded-full my-8"></div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="px-20 pt-10 text-3xl">Products</div>
+        <FilterProductBar
+          handleName={handleName}
+          handleSearchType={handleSearchType}
+          handleMaxPrice={handleMaxPrice}
+          handleMinPrice={handleMinPrice}
+          nameInput={nameInput}
+          searchType={searchType}
+          maxPrice={maxPrice}
+          minPrice={minPrice}
+        />
+        <hr className="h-1 w-4/5 color-gold mt-4"></hr>
+        <ProductWrapper product={showProduct} />
+
+        {/* Reference  : https://github.com/isaurssaurav/react-pagination-js */}
+        <ReactPagination
+          currentPage={currentPage}
+          totalSize={allProduct?.length}
+          changeCurrentPage={paginate}
+          sizePerPage={productPerPage}
+        />
+        <hr className="h-1 w-4/5 color-gold mt-4"></hr>
       </div>
     </ContentWithSidebarLayout>
   );
