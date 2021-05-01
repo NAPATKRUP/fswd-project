963198a90d32743c650c0e280c2bea403d392920
gearprn <gearprn@gmail.com>
2021-05-01 20:44:43 +0700

    :wrench: Add customerOrders resolver

--- backend/src/graphql/queries/order.js
+++ backend/src/graphql/queries/order.js
@@ -1,4 +1,21 @@
-import { OrderTC } from '../../models/order';
+import { schemaComposer } from 'graphql-compose';
+
+import { OrderModel, OrderTC } from '../../models/order';
 
 export const orderById = OrderTC.getResolver('findById');
 export const orderByMany = OrderTC.getResolver('findMany');
+
+export const customerOrders = schemaComposer.createResolver({
+  name: 'customerOrders',
+  type: [OrderTC.getType()],
+  resolve: async ({ context }) => {
+    // const { _id } = context.user;
+    try {
+      const _id = '6086470c1a67f5279c406ab0';
+      const result = await OrderModel.find({ userId: _id });
+      return result;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+});
