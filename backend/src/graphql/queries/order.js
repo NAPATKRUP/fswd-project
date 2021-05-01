3ea36b4ad9cfa8febe95df6f7d4a575bfc204af1
gearprn <gearprn@gmail.com>
2021-05-01 20:50:20 +0700

    :wrench: Update remove try catch

--- backend/src/graphql/queries/order.js
+++ backend/src/graphql/queries/order.js
@@ -11,13 +11,9 @@ export const customerOrders = schemaComposer.createResolver({
   type: [OrderTC.getType()],
   resolve: async ({ context }) => {
     // const { _id } = context.user;
-    try {
-      const _id = '6086470c1a67f5279c406ab0';
-      const result = await OrderModel.find({ userId: _id });
-      return result;
-    } catch (e) {
-      console.log(e);
-    }
+    const _id = '6086470c1a67f5279c406ab0';
+    const result = await OrderModel.find({ userId: _id });
+    return result;
   },
 });
 
