6939a4c32df47a94951dab517461012b26e3024d
napat <napatkrup@hotmail.com>
2021-05-02 19:03:21 +0700

    :zap: update resolver dashboard and fix color

--- backend/src/graphql/queries/order.js
+++ backend/src/graphql/queries/order.js
@@ -1,6 +1,9 @@
+import { ValidationError } from 'apollo-server-express';
 import { schemaComposer } from 'graphql-compose';
+import moment from 'moment';
+import 'moment/locale/th';
 
-import { OrderModel, OrderTC } from '../../models/order';
+import { OrderModel, OrderTC, CartModel } from '../../models';
 import { requiredAuth } from '../middlewares';
 
 export const orderById = OrderTC.getResolver('findById');
@@ -32,6 +35,42 @@ export const orderByIdOfUser = schemaComposer
       const { _id: userId } = context.user;
       const order = await OrderModel.findOne({ _id: orderId, userId });
       return order;
+    },
+  })
+  .wrapResolve(requiredAuth);
+
+const orderSummaryPayload = schemaComposer.createObjectTC({
+  name: 'orderSummaryPayload',
+  fields: {
+    date: 'String',
+    total: 'Float',
+  },
+});
+export const orderSummary = schemaComposer
+  .createResolver({
+    name: 'orderSummary',
+    kind: 'query',
+    type: [orderSummaryPayload],
+    resolve: async ({ context }) => {
+      const { role } = context.user;
+      if (role !== 'admin') {
+        throw new ValidationError('โปรดใช้บัญชีแอดมินในการเข้าใช้งาน');
+      }
+      const orders = await OrderModel.find({
+        $or: [{ orderStatus: 'SUCCESS' }, { orderStatus: 'SHIPPING' }, { orderStatus: 'ARRIVED' }],
+      }).sort({ checkoutAt: 1 });
+      const listOfDate = [];
+      for (const order of orders) {
+        const newDate = moment(order.checkoutAt).format('L');
+        const cart = await CartModel.findById(order.cartId);
+        const findInArrray = listOfDate.find((x) => x.date === newDate);
+        if (findInArrray) {
+          findInArrray.total += cart.totalFinalPrice;
+        } else {
+          listOfDate.push({ date: newDate, total: cart.totalFinalPrice });
+        }
+      }
+      return listOfDate;
     },
   })
   .wrapResolve(requiredAuth);
