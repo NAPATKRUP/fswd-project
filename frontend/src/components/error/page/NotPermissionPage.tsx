3d938fa75035fa2b3f440dcad61f0656f9b3aefa
Icyscools <meet.ice1@gmail.com>
2021-05-03 02:14:07 +0700

    :pencil2: Fix typo not permission page

--- frontend/src/components/error/page/NotPermissionPage.tsx
+++ frontend/src/components/error/page/NotPermissionPage.tsx
@@ -2,6 +2,6 @@ import { FC, lazy } from 'react';
 
 const ErrorStatusCard = lazy(() => import('../components/ErrorStatusCard'));
 
-const NotPermissionPage: FC = () => <ErrorStatusCard title="คุณไม่มีสิทธ์ในการเข้าใช้งานส่วนนี้" />;
+const NotPermissionPage: FC = () => <ErrorStatusCard title="คุณไม่มีสิทธิในการเข้าใช้งานส่วนนี้" />;
 
 export default NotPermissionPage;
