d4847e892515a4c7bcd3efd37c6951a72f59742d
gearprn <gearprn@gmail.com>
2021-05-03 08:11:23 +0700

    :wrench: Update route & remove unused code

--- frontend/src/components/customer/page/CustomerPage.tsx
+++ frontend/src/components/customer/page/CustomerPage.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect, useMemo, useState } from 'react';
-import { Route, Switch, useHistory, useLocation, useRouteMatch } from 'react-router';
-import { Redirect } from 'react-router-dom';
+import { FC } from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router';
 import { useSession } from '../../../context/SessionContext';
 import ContentWithSidebarLayout from '../../commons/layouts/ContentWithSidebarLayout';
 import CustomerDashboardPage from './CustomerDashboardPage';
@@ -10,14 +9,14 @@ import CustomerInfo from './CustomerInfoPage';
 import CustomerCreateAddressPage from './CustomerCreateAddressPage';
 import CustomerAddress from './CustomerAddressPage';
 import CustomerEditAddressPage from './CustomerEditAddressPage';
-import Loading from '../../commons/loading/Loading';
 import NeedAuthenticationPage from '../../error/page/NeedAuthenticationPage';
+import CustomerPaymentPage from './CustomerPaymentPage';
+import CustomerCreatePaymentPage from './CustomerCreatePaymentPage';
+import CustomerEditPaymentPage from './CustomerEditPaymentPage';
 
 const Customer: FC = () => {
-  const { user, loading } = useSession();
+  const { user } = useSession();
   const { path } = useRouteMatch();
-
-  // if (loading) return <Loading />;
 
   return user ? (
     <ContentWithSidebarLayout>
@@ -34,11 +33,18 @@ const Customer: FC = () => {
         <Route path={`${path}/address`}>
           <CustomerCreateAddressPage />
         </Route>
+        <Route path={`${path}/payment/:paymentId`}>
+          <CustomerEditPaymentPage />
+        </Route>
+        <Route path={`${path}/payment`}>
+          <CustomerCreatePaymentPage />
+        </Route>
         <Route exact path={path}>
           <CustomerDashboardPage />
           <div className="lg:px-20 md:px-10 px-4">
             <CustomerInfo />
             <CustomerAddress />
+            <CustomerPaymentPage />
           </div>
         </Route>
       </Switch>
